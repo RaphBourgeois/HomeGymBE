e1fd913a50df772ab51d7fa8c673bf41bbe804c8
RaphBourgeois <raphal.bourgeois@gmail.com>
2025-06-27 21:47:02 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,10 +2,12 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+origins = ["https://685e8bc9c651b36cdd948228--homegymfe.netlify.app:8080", "https://685e8bc9c651b36cdd948228--homegymfe.netlify.app/","https://homegymfe.netlify.app/","https://homegymfe.netlify.app:8080"]
+origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://685e8bc9c651b36cdd948228--homegymfe.netlify.app:8080", "https://685e8bc9c651b36cdd948228--homegymfe.netlify.app/","https://homegymfe.netlify.app/","https://homegymfe.netlify.app:8080"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
