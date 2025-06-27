220531d5d9e626985f796a10545aba97a705ea06
RaphBourgeois <raphal.bourgeois@gmail.com>
2025-06-27 15:25:53 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080", "100.116.235.12:5000","http://127.0.0.1:8080","http://172.27.0.2:8080", "http://172.27.0.3:8080","https://homegymbe.onrender.com:8080", "https://685e8bc9c651b36cdd948228--homegymfe.netlify.app:8080", "https://685e8bc9c651b36cdd948228--homegymfe.netlify.app/"],
+    allow_origins=["https://685e8bc9c651b36cdd948228--homegymfe.netlify.app:8080", "https://685e8bc9c651b36cdd948228--homegymfe.netlify.app/",https://homegymfe.netlify.app/,https://homegymfe.netlify.app:8080],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
