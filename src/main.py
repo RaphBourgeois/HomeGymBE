96c3e9885a0da0d8432e814b50048e1e609c8690
RaphBourgeois <raphal.bourgeois@gmail.com>
2025-06-27 14:41:26 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080", "100.116.235.12:5000","http://127.0.0.1:8080","http://172.27.0.2:8080", "http://172.27.0.3:8080"],
+    allow_origins=["http://localhost:8080", "100.116.235.12:5000","http://127.0.0.1:8080","http://172.27.0.2:8080", "http://172.27.0.3:8080","https://homegymbe.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
