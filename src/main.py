937a86d672003ffe7343b77b855c06e094f96aea
RaphBourgeois <raphal.bourgeois@gmail.com>
2025-06-27 22:04:57 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 origins = ["https://685e8bc9c651b36cdd948228--homegymfe.netlify.app:8080", "https://685e8bc9c651b36cdd948228--homegymfe.netlify.app/","https://homegymfe.netlify.app/","https://homegymfe.netlify.app:8080"]
-origins = ["*"]
+
 
 app.add_middleware(
     CORSMiddleware,
